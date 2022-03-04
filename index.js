35fd72024a259cf7ae850482456059812f7ca7c1
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-03-04 13:26:14 -0500

    commit tree 20af8f35b4a25b99d48844224e2b288790ad596a

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment (x) {
+    x++
+    return x;
+}
+
+function decrement (x) {
+    x--
+    return x;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n, 10);
+}
